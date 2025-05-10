69765fb2931be656f4ce70cb674bf2bbf7d0418d
Rittik Hasan <munshirittik03@gmail.com>
2025-05-11 01:34:09 +0600

    Add files via upload

---
+++ js/contact.js
@@ -0,0 +1,4 @@
+
+function submited(){
+    document.getElementById('show').innerHTML = "<p>Messege Sent Successfully. <br/> Thanks for your kind response.</p>"
+}
---
+++ js/script.js
@@ -0,0 +1,36 @@
+
+function reply1(){
+    
+    const messege = "<h5>added to cart.</h5>";
+    document.getElementById('show1').innerHTML = messege;
+}
+
+function reply2(){
+    
+    const messege = "<h5>added to cart.</h5>";
+    document.getElementById('show2').innerHTML = messege;
+}
+
+function reply3(){
+    
+    const messege = "<h5>added to cart.</h5>";
+    document.getElementById('show3').innerHTML = messege;
+}
+
+function reply4(){
+    
+    const messege = "<h5>added to cart.</h5>";
+    document.getElementById('show4').innerHTML = messege;
+}
+
+function reply5(){
+    
+    const messege = "<h5>added to cart.</h5>";
+    document.getElementById('show5').innerHTML = messege;
+}
+
+function reply6(){
+    
+    const messege = "<h5>added to cart.</h5>";
+    document.getElementById('show6').innerHTML = messege;
+}
